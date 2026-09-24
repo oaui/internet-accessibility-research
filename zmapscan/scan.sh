af72a9953991469a2f7efc3c1765b006fb0aab1f
Al <scrollgod1010@outlook.com>
2026-09-24 21:29:25 +0200

    Update scan.sh

--- zmapscan/scan.sh
+++ zmapscan/scan.sh
@@ -1,3 +1,3 @@
-../zmap/src/zmap -p 80 -w turk.txt -M tcp_pshack \
+../zmap/build/src/zmap -p 80 -w turk.txt -M tcp_pshack \
   --probe-args=$'text:GET / HTTP/1.1\r\nHost: youporn.com\r\n\r\n' \
   -o Turkey.txt
